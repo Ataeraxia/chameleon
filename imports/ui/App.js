f85ac2543b91a75b08e596cca3283f9901797046
Lucas Melin <lucas.melin@gmail.com>
2018-02-21 15:52:40 -0500

    Initial commit'

---
+++ imports/ui/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+// App component - represents the whole app
+export default class App extends Component {
+
+  componentDidMount(){
+    }
+
+  render() {
+    return(
+      <Router>
+        <Root>   
+          <Main>
+            <Route exact={true} path="/" render={() => <Frame title='Situation'/>}/>
+            <Route exact={true} path="/mood" render={() => <Frame title='Mood'/>}/>
+          </Main>
+        </Root>
+      </Router>
+    )
+  }
+}
+
+const Frame = ({ title }) => (
+  <h1>
+    {title}
+  </h1>
+)
+
+
+
+class LoadFile extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      content: null
+    }
+  }
+  componentDidMount = () => {
+    fetch(this.props.url)
+      .then(res => res.text())
+      .then(res => {
+        this.setState({
+          content: res
+        })
+      })
+  }
+  
+  render() {
+    const {content} = this.state
+    return (
+      <pre>
+        <div dangerouslySetInnerHTML={{__html: content}} />
+      </pre>
+    )
+  }
+}
+
+
+const Root = (props) => (
+  <div style={{
+    display: 'flex'
+  }} {...props} ></div>
+)
+
+
+const Sidebar = (props) => (
+  <div style={{
+    width: '33vw',
+    height: '100vh',
+    overflow: 'auto',
+    background: '#eee'
+  }} {...props} ></div>
+)
+
+const SidebarItem = (props) => (
+  <div style={{
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    padding: '5px 10px'
+  }} {...props}></div>
+
+)
+
+
+const Main = (props) => (
+  <div style={{
+    flex: 1,
+    height: '100vh',
+    overflow: 'auto'
+  }} >
+    <div style={{ padding: '20px' }} {...props}></div>
+  </div>
+)
